c5d1126a54ffbc5476deb6ace649b5d9d70d14bf
Charles Galvin <charles@c2dev.com.au>
2018-05-31 10:19:45 +1000

    Make base template use the Form Helper

--- minicskeleton.php
+++ minicskeleton.php
@@ -32,7 +32,8 @@ class MinicSkeleton extends Module
 		$this->version = '1.0';
 		$this->author = 'minic studio';
 		$this->need_instance = 0;
-		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6'); 
+		$this->bootstrap = true;
+		$this->ps_versions_compliancy = array('min' => '1.7', 'max' => '1.7');
 		// $this->dependencies = array('blockcart');
 
 		parent::__construct();
@@ -86,11 +87,88 @@ class MinicSkeleton extends Module
 
 	/**
  	 * admin page
-	 */	
-	public function getContent()
-	{
-		return $this->display(__FILE__, 'views/templates/admin/minicskeleton.tpl');
-	}
+	 */
+    public function getContent()
+    {
+        $output = null;
+
+        if (Tools::isSubmit('submit'.$this->name))
+        {
+            $my_module_name = strval(Tools::getValue('minicskeleton'));
+            if (!$my_module_name
+                || empty($my_module_name)
+                || !Validate::isGenericName($my_module_name))
+                $output .= $this->displayError($this->l('Invalid Configuration value'));
+            else
+            {
+                Configuration::updateValue('MYMODULE_NAME', $my_module_name);
+                $output .= $this->displayConfirmation($this->l('Settings updated'));
+            }
+        }
+        return $output.$this->displayForm();
+    }
+
+
+    public function displayForm()
+    {
+        // Get default language
+        $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+        // Init Fields form array
+        $fields_form[0]['form'] = array(
+            'legend' => array(
+                'title' => $this->l('Settings'),
+            ),
+            'input' => array(
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Configuration value'),
+                    'name' => 'minicskeleton',
+                    'size' => 20,
+                    'required' => true
+                )
+            ),
+            'submit' => array(
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            )
+        );
+
+        $helper = new HelperForm();
+
+        // Module, token and currentIndex
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+        // Language
+        $helper->default_form_language = $default_lang;
+        $helper->allow_employee_form_lang = $default_lang;
+
+        // Title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->toolbar_btn = array(
+            'save' =>
+                array(
+                    'desc' => $this->l('Save'),
+                    'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                        '&token='.Tools::getAdminTokenLite('AdminModules'),
+                ),
+            'back' => array(
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            )
+        );
+
+        // Load current value
+        $helper->fields_value['minicskeleton'] = Configuration::get('minicskeleton');
+
+        return $helper->generateForm($fields_form);
+    }
 
 	// BACK OFFICE HOOKS
 
